71f483412e8fe3956b1e04817630227e3fc7fd97
Edgar-studio <edgaravakimyan7@gmail.com>
2025-07-14 19:07:02 +0200

    repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/repo-name/', // փոխարինիր քո repository-ի անունով
+  base: '/AI/', // փոխարինիր քո repository-ի անունով
   plugins: [react(), tailwindcss()],
 });
