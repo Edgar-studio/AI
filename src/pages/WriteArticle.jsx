565496b30cc94e47736938d36f3211d62f0ea24d
Edgar-studio <edgaravakimyan7@gmail.com>
2025-07-22 18:56:46 +0200

    s

--- src/pages/WriteArticle.jsx
+++ src/pages/WriteArticle.jsx
@@ -2,6 +2,14 @@ import React from "react"
 import {Sparkles} from "lucide-react";
 
 const WriteArticle = () => {
+
+
+    const articleLenght = [
+        {length: 800, text: 'Short (500-800 words)'},
+        {length: 1200, text: 'Medium (800-1200 words)'},
+        {length: 1600, text: 'Long (1200+ words)'},
+    ]
+
     return (
         <div className="h-full overflow-y-scroll text-sm p-6 flex items-start flex-wrap gap-4
         text-slate-700">
@@ -11,6 +19,20 @@ const WriteArticle = () => {
                    <Sparkles  className="w-6 text-[#4A7AFF]"/>
                    <h1 className="text-xl font-semibold">Article Configuration</h1>
                </div>
+                <p className="mt-6 text-sm font-medium">Article Topic</p>
+
+                <input type="text" className="w-full p-2 px-3 mt-2 outline-none text-sm
+                rounded-md border border-gray-300" placeholder="The future of artificial intelligence is..."
+                 required/>
+
+                <p className="mt-4 text-sm font-medium">Article Length</p>
+
+                <div className="mt-3 flex gap-3 flex-wrap sm:max-w-9/11">
+                    {articleLenght.map((item, index)=>(
+                        <span className="text-xs px-4 py-1 border rounded-full
+                        cursor-pointer"  key={index}>{item.text}</span>
+                    ))}
+                </div>
             </form>
             {/*right col*/}
             <div>
